e4a625b2502f2322dd8427f2e18ec989ea40f73e
acuyo <acuyo84@naver.com>
2018-05-09 21:52:08 +0900

    두 번째 수정 v1.1

--- download2-8-2.py
+++ download2-8-2.py
@@ -4,7 +4,7 @@ import urllib.parse as rep
 import sys
 import io
 import os
-
+print(hi)
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
